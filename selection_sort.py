a66bf8b20cf69d9a11620bda695a5e4fb80ebec5
Sadisha Galappatti <sadishag@gmail.com>
2019-03-27 21:39:02 -0400

    Adding question from text

--- selection_sort.py
+++ selection_sort.py
@@ -3,6 +3,16 @@
 # Date: March 27, 2019
 # Algorithms textbook (CLRS) algorithms practice
 
+# 2.2-2
+# Consider sorting n numbers stored in array A by first finding the smallest element
+# of A and exchanging it with the element in AŒ1. Then find the second smallest
+# element of A, and exchange it with AŒ2. Continue in this manner for the first n1
+# elements of A. Write pseudocode for this algorithm, which is known as selection
+# sort. What loop invariant does this algorithm maintain? Why does it need to run
+# for only the first n  1 elements, rather than for all n elements? Give the best-case
+# and worst-case running times of selection sort in ‚-notation.
+
+
 
 # loops through the array, searches for minimum number and switches
 # with the first value
